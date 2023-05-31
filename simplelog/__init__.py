59964b9bb7a3917f82ddf14bc100d51a11d297d7
mikigo <huangmingqiang@uniontech.com>
2023-05-31 18:21:47 +0800

    update

--- simplelog/__init__.py
+++ simplelog/__init__.py
@@ -2,19 +2,26 @@ import inspect
 import logging
 import os
 import re
+import sys
 import threading
 import time
 import weakref
 from functools import wraps
 from inspect import _ParameterKind
 
-from allure_commons._allure import StepContext
-from allure_commons.utils import func_parameters, represent
+try:
+    from allure_commons._allure import StepContext
+    from allure_commons.utils import func_parameters, represent
+
+    ALLURE_STEP = True
+except ModuleNotFoundError:
+    ALLURE_STEP = False
+
 from simplelog.conf import setting
 
 
 class Singleton(type):
-    """底层单例模式封装"""
+    """单例模式"""
 
     _instance_lock = threading.Lock()
 
@@ -52,7 +59,6 @@ def is_static_method(klass_or_instance, attr: str):
     if attr.startswith("_"):
         return False
     value = getattr(klass_or_instance, attr)
-
     # is a function or method
     if inspect.isroutine(value):
         if isinstance(value, property):
@@ -68,66 +74,85 @@ def is_static_method(klass_or_instance, attr: str):
         # Can't be a regular method, must be a static method
         if len(args) == 0:
             return True
-
         # must be a regular method
-        elif args[0] == "self":
+        if args[0] == "self":
             return False
-
-        else:
-            return inspect.isfunction(value)
-
+        return inspect.isfunction(value)
     return False
-
-
 def _trace(func):
+    # pylint: disable=R0912
     @wraps(func)
     def wrapped(*a, **kw):
         try:
-            try:
-                # 对象实例化后调用类方法报错处理
-                if (
-                        isinstance(a[0], inspect._findclass(func))
-                        and func.__name__ != "__init__"
+            # 对象实例化后调用类方法报错处理
+            if (
+                # pylint: disable=protected-access
+                isinstance(a[0], inspect._findclass(func))
+                and func.__name__ != "__init__"
+            ):
+                if func:
+                    if any(
+                        [
+                            inspect.ismethod(func),
+                            is_static_method(
+                                # pylint: disable=protected-access
+                                inspect._findclass(func),
+                                func.__name__,
+                            ),
+                        ]
+                    ):
+                        a = list(a)[1:]
+        except IndexError:
+            pass
+        params = func_parameters(func, *a, **kw)
+        # pylint: disable=unnecessary-lambda
+        args = list(map(lambda x: represent(x), a))
+        if func.__doc__:
+            if not func.__name__.startswith("_"):
+                # 处理多行注释时候，换行空格过多
+                title = re.split(":param|@param|@return|:return", func.__doc__)[0]
+                title = "".join([ln.strip() for ln in title.split("\n")])
+                params_text = {}
+                # 获取方法的所有参数，并行程，{形参：实参} 的字典
+                for index, param in enumerate(
+                    inspect.signature(func).parameters.values()
                 ):
-                    if func:
-                        if any(
-                                [
-                                    inspect.ismethod(func),
-                                    is_static_method(inspect._findclass(func), func.__name__)
-                                ]
-                        ):
-                            a = list(a)[1:]
-            except IndexError:
-                pass
-            params = func_parameters(func, *a, **kw)
-            args = list(map(lambda x: represent(x), a))
-            if func.__doc__:
-                if not func.__name__.startswith("_"):
-                    ln = "\n"
-                    title = re.split(":param|@param|@return|:return", func.__doc__)[0].strip().replace(ln, "")
-                else:
-                    return func(*a, **kw)
-            else:
-                title = func.__name__
-            if func.__name__ != "__init__":
-                with StepContext(title.format(*args, **params), params):
-                    return func(*a, **kw)
+                    if param.name == "self":
+                        continue
+                    params_text[param.name] = param.default
+                    if args:
+                        try:
+                            params_text[param.name] = args[index]
+                        except IndexError:
+                            pass
+                    if kw:
+                        try:
+                            params_text[param.name] = kw[param.name]
+                        except KeyError:
+                            pass
+                # 文本替换
+                for parameter, argument in params_text.items():
+                    if f"{parameter}" in title:
+                        if argument:
+                            title = title.replace(
+                                f"{{{{{parameter}}}}}",
+                                argument.strip("'")
+                                if isinstance(argument, str)
+                                else str(argument),
+                            )
+                        else:
+                            title = title.replace(f"{{{{{parameter}}}}}", "")
             else:
                 return func(*a, **kw)
-        finally:
-            if func.__doc__:
-                if not func.__name__.startswith("_"):
-                    ln = "\n"
-                    logger.info(
-                        f'[{func.__name__}]: {re.split(":param|@param|@return|:return", func.__doc__)[0].strip().replace(ln, "")}'
-                    )
-            else:
-                if func.__name__ != "__init__":
-                    logger.error(f"操作日志缺失:[{func.__name__}]")
-
+        else:
+            title = func.__name__
+        logger.info(f"[{func.__name__}]: " f"{title}", auteadd=False)
+        if func.__name__ != "__init__":
+            with StepContext(title, params):
+                return func(*a, **kw)
+        else:
+            return func(*a, **kw)
     return wrapped
-
-
 def log(cls):
     """
     类日志装饰器
@@ -135,48 +160,70 @@ def log(cls):
     :return:
     """
     for name, obj in inspect.getmembers(
-            cls, lambda x: inspect.isfunction(x) or inspect.ismethod(x)
+        cls, lambda x: inspect.isfunction(x) or inspect.ismethod(x)
     ):
         try:
-            class_name = obj.__qualname__.split('.')[0]
+            class_name = obj.__qualname__.split(".")[0]
         except AttributeError:
             class_name = obj.__self__.__name__
-        if class_name.endswith("Widget"):
-            if name == "__init__":
-                continue
-            setattr(cls, name, _trace(obj))
-        elif class_name.startswith("Assert") or class_name == "ShortCut":
-            if class_name == cls.__name__:
+        if name.startswith("_"):
+            continue
+        # else:
+        if (
+            class_name.startswith(setting.CLASS_NAME_STARTSWITH)
+            or class_name.endswith(setting.CLASS_NAME_ENDSWITH)
+            or any(
+                (class_name.find(text) > -1 for text in setting.CLASS_NAME_CONTAIN)
+            )
+        ):
+            if hasattr(getattr(cls, name), "__log"):
+                if not getattr(cls, name).__log:
+                    setattr(cls, name, _trace(obj))
+                    setattr(getattr(cls, name), "__log", True)
+            else:
                 setattr(cls, name, _trace(obj))
-
+                setattr(getattr(cls, name), "__log", True)
     return cls
-
-
 class _ColoredFormatter(logging.Formatter):
     def formatMessage(self, record: logging.LogRecord) -> str:
         """Format a message from a record object."""
         if record.levelname == "INFO":
             record.levelname = "\033[1;97mINFO \033[0m"  # 白色
-            record.message = re.sub(r"(\[[a-zA-Z_]*?])?(.+)", r"\033[1;32m\1\033[0m\033[1;97m\2\033[0m", record.message,
-                                    count=1)
+            record.message = re.sub(
+                r"(\[[a-zA-Z_]*?])?(.+)",
+                r"\033[1;32m\1\033[0m\033[1;97m\2\033[0m",
+                record.message,
+                count=1,
+            )
             # record.message = f"\033[1;97m{record.message}\033[0m"
         elif record.levelname == "ERROR":
             record.levelname = "\033[1;31mERROR\033[0m"  # 红色
-            record.message = f"\033[1;31m{record.message}\033[0m"
+            # record.message = f"\033[1;31m{record.message}\033[0m"
+            record.message = re.sub(
+                r"(\[[a-zA-Z_]*?])?(.+)",
+                r"\033[1;32m\1\033[0m\033[1;31m\2\033[0m",
+                record.message,
+                count=1,
+            )
         elif record.levelname == "DEBUG":
             record.levelname = "\033[1;94mDEBUG\033[0m"  # 蓝色
-            record.message = f"\033[1;94m{record.message}\033[0m"
+            record.message = re.sub(
+                r"(\[[a-zA-Z_]*?])?(.+)",
+                r"\033[1;32m\1\033[0m\033[1;94m\2\033[0m",
+                record.message,
+                count=1,
+            )
         message = super().formatMessage(record)
         return message
-
-
+# pylint: disable=R0903
 class IgnoreFilter(logging.Filter):
+    """IgnoreFilter"""
     def filter(self, record):
         return record.name not in ("PIL.PngImagePlugin", "easyprocess")
-
-
+# pylint: disable=invalid-name
 class logger(metaclass=Singleton):
-
+    """logger"""
+    __author__ = "Litao <litaoa@uniontech.com>"
     def __init__(self, level):
         """
         日志配置
@@ -188,10 +235,10 @@ class logger(metaclass=Singleton):
         if not os.path.exists(log_path):
             os.makedirs(log_path)
         log_path_debug = (
-                log_path + rf'/{time.strftime("%Y-%m-%d", time.localtime())}_debug.log'
+            log_path + rf'/{time.strftime("%Y-%m-%d", time.localtime())}_debug.log'
         )  # debug级别日志路径
         logfile_error = (
-                log_path + rf'/{time.strftime("%Y-%m-%d", time.localtime())}_error.log'
+            log_path + rf'/{time.strftime("%Y-%m-%d", time.localtime())}_error.log'
         )  # error级别日志路径
         # 路径不存在则创建
         if os.path.exists(log_path_debug):
@@ -216,21 +263,24 @@ class logger(metaclass=Singleton):
             else setting.USERNAME
         )
         self.date_format = "%m/%d %H:%M:%S"
-        self.log_format = f"{self.sys_arch}-{self.user_name}{self.ip_flag}: %(asctime)s | %(levelname)s | %(module)s: %(message)s"
+        self.log_format = (
+            f"{self.sys_arch}-{self.user_name}{self.ip_flag}: "
+            "%(asctime)s | %(levelname)s | %(message)s"
+        )
         self.logger = logging.getLogger()
-        # self.logger.setLevel(level)  # Log等级总开关
+        self.logger.setLevel(level)  # Log等级总开关
         self.logger.addFilter(IgnoreFilter())
-        fh = logging.FileHandler(log_path_debug, mode="w+")
-        fh.setLevel(logging.DEBUG)  # 输出到file的log等级的开关
-        fh.addFilter(IgnoreFilter())
-        fh.setFormatter(
+        _fh = logging.FileHandler(log_path_debug, mode="w+")
+        _fh.setLevel(logging.DEBUG)  # 输出到file的log等级的开关
+        _fh.addFilter(IgnoreFilter())
+        _fh.setFormatter(
             logging.Formatter(
                 self.log_format,
                 datefmt=self.date_format,
             )
         )
         fh_error = logging.FileHandler(logfile_error, mode="w+")
-        fh.setFormatter(
+        _fh.setFormatter(
             logging.Formatter(
                 self.log_format,
                 datefmt=self.date_format,
@@ -238,46 +288,63 @@ class logger(metaclass=Singleton):
         )
         fh_error.setLevel(logging.ERROR)  # 输出到file的log等级的开关
         # 第四步，再创建一个handler，用于输出到控制台
-        ch = logging.StreamHandler()
-        ch.setLevel(level)  # 输出到console的log等级的开关
-        ch.addFilter(IgnoreFilter())
+        _ch = logging.StreamHandler()
+        _ch.setLevel(level)  # 输出到console的log等级的开关
+        _ch.addFilter(IgnoreFilter())
         # 第五步，定义handler的输出格式
         formatter = _ColoredFormatter(
-            f"\033[1;31m{self.sys_arch}-{self.user_name}{self.ip_flag}\033[0m: \033[93m%(asctime)s\033[0m | %(levelname)s | \033[36m%(module)s\033[0m: %(message)s",
+            (
+             f"\033[1;31m{self.sys_arch}-{self.user_name}{self.ip_flag}\033[0m: "
+             "\033[93m%(asctime)s\033[0m | %(levelname)s | %(message)s"
+            ),
             datefmt=self.date_format,
         )
-        ch.setFormatter(formatter)
+        _ch.setFormatter(formatter)
         # 第六步，将logger添加到handler里面
-        self.logger.addHandler(fh)
+        self.logger.addHandler(_fh)
         self.logger.addHandler(fh_error)
-        self.logger.addHandler(ch)
-        # logging.basicConfig(level=logging.DEBUG,
-        #                     format=f'{self.sys_arch}-{self.user_name}{self.ip_flag}: %(asctime)s | %(levelname)s | %(module)s: %(message)s',
-        #                     datefmt='%a, %d %b %Y %H:%M:%S',
-        #                     filename=log_path_debug,
-        #                     filemode='w+')
-        # logging.basicConfig(level=logging.ERROR,
-        #                     format=f'{self.sys_arch}-{self.user_name}{self.ip_flag}: %(asctime)s | %(levelname)s | %(module)s: %(message)s',
-        #                     datefmt='%a, %d %b %Y %H:%M:%S',
-        #                     filename=logfile_error,
-        #                     filemode='w+')
-
+        self.logger.addHandler(_ch)
     @staticmethod
-    def info(message):
+    def info(message, auteadd=True):
+        """info"""
+        if len(logging.root.handlers) == 0:
+            logger(setting.LOG_LEVEL)
+        if auteadd:
+            current_frame = sys._getframe(1) if hasattr(sys, "_getframe") else None
+            message = f"[{current_frame.f_code.co_name}]: {message}"
         logging.info(message)
-
     @staticmethod
-    def debug(message):
-        logging.debug(message)
-
+    def debug(message, auteadd=True):
+        """debug"""
+        if len(logging.root.handlers) == 0:
+            logger(setting.LOG_LEVEL)
+        if auteadd:
+            current_frame = sys._getframe(1) if hasattr(sys, "_getframe") else None
+            message = f"[{current_frame.f_code.co_name}]: {message}"
+        # 判断是否用例直接调用底层基础方法，则输出info日志
+        current_frame1 = sys._getframe(2) if hasattr(sys, "_getframe") else None
+        if current_frame1.f_code.co_name.startswith("test_"):
+            logging.info(message)
+        else:
+            logging.debug(message)
     @staticmethod
-    def error(message):
+    def error(message, auteadd=True):
+        """error"""
+        if len(logging.root.handlers) == 0:
+            logger(setting.LOG_LEVEL)
+        if auteadd:
+            current_frame = sys._getframe(1) if hasattr(sys, "_getframe") else None
+            message = f"[{current_frame.f_code.co_name}]: {message}"
         logging.error(message)
-
     @staticmethod
     def exception(message):
+        """exception"""
+        if len(logging.root.handlers) == 0:
+            logger(setting.LOG_LEVEL)
         logging.exception(message)
-
     @staticmethod
     def warning(message):
+        """warning"""
+        if len(logging.root.handlers) == 0:
+            logger(setting.LOG_LEVEL)
         logging.warning(message)
--- simplelog/conf.py
+++ simplelog/conf.py
@@ -5,10 +5,14 @@ from platform import machine
 
 class _Setting:
 
-    ROOT_DIR = os.path.dirname(os.path.abspath(__file__))
-    REPORT_PATH = os.path.join(ROOT_DIR, "logs")
-    HOST_IP = str(os.popen("hostname -I |awk '{print $1}'").read()).strip("\n").strip()
     SYS_ARCH = machine()
     USERNAME = getuser()
+    ROOT_DIR = os.path.dirname(os.path.abspath(__file__))
+    # 日志文件生成的路径
+    LOG_PATH = os.path.join(ROOT_DIR, "logs")
+    # 本机IP
+    HOST_IP = str(os.popen("hostname -I |awk '{print $1}'").read()).strip("\n").strip()
+
+    LOG_LEVEL = "DEBUG"
 
 setting = _Setting()
